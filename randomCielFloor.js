5d2ba4e2f868fef56cac3b063747e1626730a73c
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-10-02 17:32:32 +0800

    I have practised absolute, random, floor, ceil and round

---
+++ randomCielFloor.js
@@ -0,0 +1,8 @@
+var number1 = -55.53;
+var result = Math.ceil(number1);
+var result2 = Math.floor(number1);
+var result3 = Math.round(number1);
+var result4 = Math.abs(number1);
+
+var number2 = Math.random()*10;
+console.log(number2);
